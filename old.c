99fc0478cef8130f11da304e5e643112b6e3b15c
Anastasiia Belosevich <anbelose@f1r9s2.clusters.42paris.fr>
2025-08-01 20:48:38 +0200

    123

--- old.c
+++ old.c
@@ -5,63 +5,62 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: anbelose <anbelose@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/01 20:35:03 by anbelose          #+#    #+#             */
-/*   Updated: 2025/08/01 20:45:52 by anbelose         ###   ########.fr       */
+/*   Created: 2025/08/01 14:01:51 by anbelose          #+#    #+#             */
+/*   Updated: 2025/08/01 14:27:55 by anbelose         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	safe_free(char **p)
+char	*get_line_remain_outil(char **remain, int i)
 {
-	if (*p)
+	char	*new_remain;
+	char	*line;
+
+	line = ft_strndup(*remain, i + 1);
+	if (!line)
 	{
-		free(*p);
-		*p = NULL;
+		free(*remain);
+		*remain = NULL;
+		return (NULL);
 	}
+	new_remain = ft_strndup((*remain) + i + 1, ft_strlen(*remain) - i - 1);
+	if (!new_remain)
+	{
+		free(*remain);
+		free(line);
+		*remain = NULL;
+		return (NULL);
+	}
+	free(*remain);
+	*remain = new_remain;
+	return (line);
 }
 
-char	*get_line1(char *remain)
+char	*get_line_and_remain(char **remain)
 {
 	size_t	i;
 	char	*line;
 
 	i = 0;
-	if (!remain || !*remain)
+	if (!*remain || !**remain)
 		return (NULL);
-	if (!ft_strchr(remain, '\n'))
+	if (!ft_strchr((*remain), '\n'))
 	{
-		line = ft_strndup(remain, ft_strlen(remain));
-		if (!line)
-			return (NULL);
+		line = ft_strndup(*remain, ft_strlen(*remain));
+		free(*remain);
+		*remain = NULL;
 		return (line);
 	}
-	while (remain[i] != '\n')
+	while ((*remain)[i] != '\n')
 		i++;
-	line = ft_strndup(remain, i + 1);
+	line = get_line_remain_outil(remain, i);
 	if (!line)
-		return (NULL);
-	return (line);
-}
-
-void	get_new_remain(char **remain, char *line)
-{
-	char	*new_remain;
-	int		len;
-
-	if (!*remain || !**remain)
-		return ;
-	if (!ft_strchr((*remain), '\n'))
-		return (safe_free(remain));
-	len = ft_strlen(line);
-	new_remain = ft_strndup(*remain + len, ft_strlen(*remain) - len);
-	if (!new_remain)
 	{
-		safe_free(remain);
-		return ;
+		free(*remain);
+		*remain = NULL;
 	}
-	free(*remain);
-	*remain = new_remain;
+	return (line);
 }
 
 void	reading(int fd, char **remain)
@@ -75,15 +74,19 @@ void	reading(int fd, char **remain)
 		return ;
 	read_res = read(fd, buf, BUFFER_SIZE);
 	if (read_res < 0)
-		return (free(buf), safe_free(remain));
+	{
+		free(*remain);
+		*remain = NULL;
+		return (free(buf));
+	}
 	while (read_res > 0)
 	{
 		buf[read_res] = '\0';
 		tmp = ft_strjoin(*remain, buf);
-		if (!tmp)
-			return (free(buf), safe_free(remain));
 		free(*remain);
 		*remain = tmp;
+		if (!tmp)
+			return (free(buf));
 		if (ft_strchr(*remain, '\n'))
 			break ;
 		read_res = read(fd, buf, BUFFER_SIZE);
@@ -107,12 +110,11 @@ char	*get_next_line(int fd)
 	}
 	if (!ft_strchr(remain, '\n'))
 		reading(fd, &remain);
-	line = get_line1(remain);
+	line = get_line_and_remain(&remain);
 	if (!line && remain)
 	{
-		safe_free(&remain);
-		return (NULL);
+		free(remain);
+		remain = NULL;
 	}
-	get_new_remain(&remain, line);
 	return (line);
 }
